7b969aec466363425102459a4f01c16b089f50ba
BasilZap <zapr9@yandex.ru>
2023-05-18 23:46:25 +0300

    added functions prepare_list для нормализации списка и удаления нестандартных записей и make_sorted_ops для сортировки списка по дате и времени по убыванию. Добавлены описания функций

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,41 @@
 import json
 
-"""
-1. считываем json
-2. формируем новый спиок из полученных записей по убыванию даты, отсортировываем отмененные операции
-3. читаем первые 5 элементов списка
-4. форматируем каждый
-5. выводим каждый по очереди
-"""
-
 
 def get_json_data():
-    pass
+    """
+    Чтение файла json
+    :return: список операций
+    """
+    with open("operations.json", "r", encoding="utf-8") as json_file:
+        raw_json = json.loads(json_file.read())
+    return raw_json
 
 
-def make_sorted_ops():
-    pass
+def prepare_list(raw_json):
+    """
+    Нормализация данных полученных из json:
+    отсеивание нестандартных операций
+    :param raw_json:
+    :return:
+    """
+    normalized_j_list = []
+    key_date = 'date'
+    key_from = 'from'
+    for j_rec in raw_json:
+        if dict(j_rec).get(key_date) and dict(j_rec).get(key_from):
+            normalized_j_list.append(j_rec)
+    return normalized_j_list
+
+
+def make_sorted_ops(raw_json):
+    """
+    Сортировка данных по дате
+    и времени по убыванию
+    :param raw_json: нормализованный список
+    :return: сортированный список
+    """
+    operation_date_time = sorted(raw_json, key=lambda x: (x['date']), reverse=True)
+    return operation_date_time
 
 
 def formatted_data():
@@ -23,3 +44,8 @@ def formatted_data():
 
 def print_data():
     pass
+
+
+
+print(make_sorted_ops(prepare_list(get_json_data())))
+
