65ad1cd3701f83608cd4f67c99c4f8188265cb26
BasilZap <zapr9@yandex.ru>
2023-05-21 18:26:52 +0300

    Добавлена функция formatted_rec(raw_json) для формирования спика значений, функции formatted_date, formatted_card, formatted_count, data_for_print переделаны для работы со списком. Добавлены описания для функций, дополнено описание в readme.md

--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ get_json_data(path) Считывает данные json из файла
 prepare_list(raw_json) Подготовка данных, если нет интересующих записей - отсеиваем
 sort_recs_by_date(raw_json) Сортируем список по дате операции
 get_x_records(raw_json, x=5) получение x последних операций (5 по умолчанию)
+formatted_rec(raw_json) создает список с необходимыми данными
 formatted_date(rec_date) форматируем данные о дате транзакции, возвращаем (f_rec_date)
 formatted_card(rec_card) форматируем данные карты, возвращаем (f_rec_card)
 formatted_count(rec_count) форматируем данные счета, возвращаем (f_rec_count)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from datetime import date
 
 JSON_DATA_PATH = "../src/operations.json"
 
+
 def get_json_data(data_path):
     """
     Чтение файла json
@@ -16,17 +17,19 @@ def get_json_data(data_path):
 def prepare_list(raw_json):
     """
     Нормализация данных полученных из json:
-    отсеивание нестандартных операций
+    отсеивание нестандартных элементов списка и операций
     :param raw_json:
-    :return:
+    :return: Список библиотек normalized_j_list
     """
     normalized_j_list = []
     key_date = 'date'
     key_from = 'from'
+    key_exec = 'state'
     for j_rec in raw_json:
         if type(j_rec) is dict:
             if dict(j_rec).get(key_date) and dict(j_rec).get(key_from):
-                normalized_j_list.append(j_rec)
+                if dict(j_rec).get(key_exec) == "EXECUTED":
+                    normalized_j_list.append(j_rec)
     return normalized_j_list
 
 
@@ -35,42 +38,86 @@ def sort_recs_by_date(raw_json):
     Сортировка данных по дате
     и времени по убыванию
     :param raw_json: нормализованный список
-    :return: сортированный список
+    :return: сортированный список библиотек operation_date_time
     """
     operation_date_time = sorted(raw_json, key=lambda x: (x['date']), reverse=True)
     return operation_date_time
 
 
-def formatted_date(json_rec):
-    j_rec = dict(json_rec)
-    # форматируем дату для вывода
-    transact_date = str(j_rec['date']).partition('T')[0]
-    non_form_date = date.fromisoformat(transact_date)
-    form_date = non_form_date.strftime("%d-%m-%Y")
+def get_x_records(raw_json, x=5):
+    """
+    Принимает сортированный список библиотек
+    возвращает первые x элементов
+    :param raw_json: сортированный список библиотек
+    :param x: Количество элементов для вывода
+    :return: Список из x элементов
+    """
+    rec_list = []
+    for rec in range(x):
+        rec_list.append(raw_json[rec])
+    return rec_list
 
-    # форматируем данные карты для вывода
-    card_list = str.split(j_rec["from"])
+
+def formatted_rec(raw_json):
+    """
+    Формирует список необходимых
+    для вывода значений
+    :param raw_json: Список элементов в формате json
+    :return: Список элементов в формате string
+    """
+    data_list = [raw_json['date'], raw_json['description'], raw_json['from'], raw_json['to'],
+                 raw_json["operationAmount"]["amount"], raw_json["operationAmount"]["currency"]["name"]]
+    return data_list
+
+
+def formatted_date(data_list):
+    """
+    Форматирует дату для вывода
+    :param data_list: список значений для вывода
+    :return: список значений для вывода
+    """
+    transact_date = str(data_list[0]).partition('T')[0]
+    non_form_date = date.fromisoformat(transact_date)
+    data_list[0] = non_form_date.strftime("%d-%m-%Y")
+    return data_list
+
+
+def formatted_card(data_list):
+    """
+    Форматирует данные карты для вывода
+    :param data_list: список значений для вывода
+    :return: список значений для вывода
+    """
+    card_list = str.split(data_list[2])
     card_num = card_list[-1]
     num_hide = card_num[:6] + (len(card_num[6:-4]) * '*') + card_num[-4:]
     num_form = num_hide[:4] + " " + num_hide[4:8] + " " + num_hide[8:12] + " " + num_hide[12:]
     card_list[-1] = num_form
+    data_list[2] = ' '.join(card_list)
+    return data_list
 
-    # форматируем номер счета для вывода
-    transaction_list = str.split(j_rec["to"])
+
+def formatted_count(data_list):
+    """
+    Форматирует номер счета для вывода
+    :param data_list: список значений для вывода
+    :return: список значений для вывода
+    """
+    transaction_list = str.split(data_list[3])
     transaction_num = transaction_list[-1]
     transaction_list[-1] = "**" + transaction_num[-4:]
+    data_list[3] = ' '.join(transaction_list)
+    return data_list
 
-    # формируем строку для вывода
-    text_record = (f"{form_date} {j_rec['description']}\n"
-                   f"{' '.join(card_list)} -> {' '.join(transaction_list)}\n"
-                   f"{j_rec['operationAmount']['amount']} "
-                   f"{j_rec['operationAmount']['currency']['name']}\n")
+
+def data_for_print(data_list):
+    """
+    Форматирует строку для вывода на экран
+    :param data_list:
+    :return:
+    """
+    f_rec_date, desc, f_rec_card, f_rec_count, amount, name = data_list
+    text_record = (f"{f_rec_date} {desc}\n"
+                   f"{f_rec_card} -> {f_rec_count}\n"
+                   f"{amount} {name}\n")
     return text_record
-
-
-def print_data():
-    pass
-
-
-x = sort_recs_by_date(prepare_list(get_json_data(JSON_DATA_PATH)))
-print(formatted_date(x[0]))
